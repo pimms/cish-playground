1ad41048865821c11e4d37e6b12c2de8faa9c105
Joakim Stien <joakim.stien@gmail.com>
2020-03-12 22:43:28 +0100

    Initial Commit

---
+++ cpg/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cpg
+//
+//  Created by pimms on 12/03/2020.
+//  Copyright © 2020 Joakim Stien. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
